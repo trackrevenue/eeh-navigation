bb893ad4a10a0ad4dd181b0f352f521ac26f2b23
danielVickTR <danielvick@trackrevenue.com>
2017-06-26 16:04:47 -0700

    Changed collapse value

--- src/navbar/eeh-navigation-navbar-directive.js
+++ src/navbar/eeh-navigation-navbar-directive.js
@@ -71,7 +71,7 @@ var NavbarDirective = function ($window, eehNavigation) {
                     return;
                 }
                 var width = (newValue.innerWidth > 0) ? newValue.innerWidth : $window.screen.width;
-                if (width >= 1048) {
+                if (width >= 1152) {
                     scope.isNavbarCollapsed = true;
                 }
             }, true);
